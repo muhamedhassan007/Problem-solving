be6873aa2f7fb80574eb6d8e000314d5ad048597
muhamed hasssan <56801636+muhamedhassan007@users.noreply.github.com>
2020-05-15 10:39:06 +0200

    Update quickfind.java

--- Dynamicconnectivity/quickfind.java
+++ Dynamicconnectivity/quickfind.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Dynamicconnectivity;
 
-/**
- *
- * @author HP
- */
+
 public class quickfind {
     protected int []id;
     quickfind(int N){
